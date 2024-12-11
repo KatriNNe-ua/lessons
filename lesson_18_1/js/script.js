3b2cb9d45df100e96ce6166a5c9e97412bd92f19
KatriNNe-ua <mazyrukkatya@gmail.com>
2024-12-11 09:43:04 +0200

    Add files via upload

---
+++ lesson_18_1/js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("click", function (e) {
+  const targetElement = e.target;
+  // .icon-menu це клас кнопки при кліку на яку буде спрацьовувати код додаватись до тегу html клас menu-open
+  if (targetElement.closest(".header__btn-menu")) {
+    document.documentElement.classList.toggle("open-menu");
+    e.preventDefault();
+  }
+});
+// let documentActions = (e) => {
+//   const targetElement = e.target;
+
+//   if (targetElement.closest(".header__btn-menu")) {
+//     document.documentElement.classList.toggle("open-menu");
+//   }
+// };
+
+// document.addEventListener("click", documentActions);
