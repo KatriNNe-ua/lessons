713dc52db8ceeeb1674e2ca052d30346a1a304bc
KatriNNe-ua <mazyrukkatya@gmail.com>
2025-02-06 21:16:38 +0200

    Add files via upload

--- js_lesson_05/js/task_15.js
+++ js_lesson_05/js/task_15.js
@@ -4,34 +4,48 @@
 //з чотирьох зображень. Вивести ці зображення і повідомити виграш користувача (три перших
 //зображення 100 грн, три других – 200, три третіх – 500, три четвертих зображення – 1000грн). Використати цикли і switch (для вибору зображення за номером).
 
-if (confirm("Почати тестування?")) {
-  // const img1 =
-  // const img2 =
-  // const img3 =
-  // const img4 =
- let res = "";
-  for (let i = 1; i <= 4; i++) {
-    const numberGenerate = 1 + Math.floor(Math.random() * 4);
-    let res ;
-    switch (numberGenerate) {
-      case 1:
-        res =
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJBmaD2_Iq0yI8koN8SolLAABuGGxqrDPaKw&s";
-        break;
-      case 2:
-        res =
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcPNzvvqxxuITz_r-1NYKbpxiDPDt-R4hKgRtprOvKZD2mYgl2l2wYdK5vvJ0NUcccyzc&usqp=CAU";
-        break;
-      case 3:
-        res =
-          "https://zefirka.club/uploads/posts/2022-09/thumbs/1664298169_19-zefirka-club-p-oboi-na-telefon-kotorie-malo-vesyat-19.jpg";
-        break;
-      case 4:
-        res =
-          "https://zefirka.club/uploads/posts/2022-09/thumbs/1664298224_25-zefirka-club-p-oboi-na-telefon-kotorie-malo-vesyat-25.jpg";
-    }
-	 document.write(`<img src=${res} width = 100px>`);
+let marker = "";
+let winningAmount = 0;
+for (let i = 1; i <= 3; i++) {
+  const numberGenerate = 1 + Math.floor(Math.random() * 4);
+  let imgGenerate;
+  marker += numberGenerate
 
+  switch (numberGenerate) {
+    case 1:
+      imgGenerate =
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJBmaD2_Iq0yI8koN8SolLAABuGGxqrDPaKw&s";
+  
+      break;
+    case 2:
+      imgGenerate =
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcPNzvvqxxuITz_r-1NYKbpxiDPDt-R4hKgRtprOvKZD2mYgl2l2wYdK5vvJ0NUcccyzc&usqp=CAU";
+    
+      break;
+    case 3:
+      imgGenerate =
+        "https://zefirka.club/uploads/posts/2022-09/thumbs/1664298169_19-zefirka-club-p-oboi-na-telefon-kotorie-malo-vesyat-19.jpg";
+
+      break;
+    case 4:
+      imgGenerate =
+        "https://zefirka.club/uploads/posts/2022-09/thumbs/1664298224_25-zefirka-club-p-oboi-na-telefon-kotorie-malo-vesyat-25.jpg";
+    
   }
-//   document.write(`<img srs=${res}>`)
+  document.write(`<img src="${imgGenerate}">`);
 }
+switch (marker) {
+  case "111":
+    winningAmount = 100;
+    break;
+  case "222":
+    winningAmount = 200;
+    break;
+  case "333":
+    winningAmount = 500;
+    break;
+  case "444":
+    winningAmount = 1000;
+    break;
+}
+document.write(`<br>Ваш виграш: ${winningAmount}грн`);
