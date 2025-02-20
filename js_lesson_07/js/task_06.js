491616421fdfbdc59c6aedc9408e52c181e25f89
KatriNNe-ua <mazyrukkatya@gmail.com>
2025-02-20 13:21:57 +0200

    Add files via upload

--- js_lesson_07/js/task_06.js
+++ js_lesson_07/js/task_06.js
@@ -6,11 +6,12 @@
 // Кілометри у милі.
 
 function getCentimetersToInches(numCm) {
-  return (numCm / 2.54).toFixed(2);
+  return (numCm * 2.54).toFixed(2);
 }
 function getKilogramsToPounds(numKg) {
-  return (numKg / 2.20462).toFixed(2);
+  return (numKg * 2.20462).toFixed(5);
 }
 function getKilometersToMiles(numKm) {
-  return (numKm / 1.60934).toFixed(2);
+  return (numKm * 1.60934).toFixed(5);
 }
+
--- js_lesson_07/js/task_07.js
+++ js_lesson_07/js/task_07.js
@@ -3,16 +3,16 @@
 //Задача 6. Створити функцію, яка створює таблицю з вказаною кількістю рядків і
 //стовпців (таблиця заповнюється заданим фіксованим повідомленням).
 
-function CreateTable(row, col) {
+function createTable(row, col) {
   document.write(`<table>`);
-  let num = 1;
+let message = "повідомлення";
   for (let tr = 1; tr <= row; tr++) {
     document.write(`<tr>`);
     for (let td = 1; td <= col; td++) {
-      document.write(`<td>повідомлення</td>`);
+      document.write(`<td>${message}</td>`);
     }
     document.write(`</tr>`);
   }
   document.write(`</table>`);
 }
-CreateTable(8, 3);
+createTable(8, 3);
